e2dac1ed61030e01345767b44dc59872881dd982
Luiz Victor Alvarenga Andrade <victorzld.andrade@gmail.com>
2025-05-08 15:51:00 -0300

    corrige o texto do /qrcode

--- src/main/java/com/luizvictor/qrcode/generator/controller/QrCodeController.java
+++ src/main/java/com/luizvictor/qrcode/generator/controller/QrCodeController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/qrocode")
+@RequestMapping("/qrcode")
 public class QrCodeController {
 
     private final QrCodeGeneratorService qrCodeGeneratorService;
